aad9f33b29607d2ecdea69796fcf1f065e3cd07c
Parkwochang <whwh3560@gmail.com>
2022-07-18 20:17:20 +0900

    recap

--- description.ts
+++ description.ts
@@ -24,3 +24,31 @@ nico.getFullName();
 (function (a) {
   return a;
 })();
+
+type Words = {
+  [key: string]: string;
+};
+
+class Dict {
+  private words: Words; // property가 constructor부터 바로 초기화되지 않는 이 부분
+  constructor() {
+    this.words = {}; // 수동으로 초기화
+  }
+  add(word: Word) {
+    if (this.words[word.term] === undefined) this.words[word.term] = word.def;
+  }
+  def(term: string) {
+    return this.words[term];
+  }
+}
+
+class Word {
+  constructor(public term: string, public def: string) {}
+}
+
+const kimchi = new Word("kimchi", "한국의 음식");
+
+const dict = new Dict();
+
+dict.add(kimchi);
+dict.def("kimchi");
