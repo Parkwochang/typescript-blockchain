c594f2cf3694f3b3e3dfd5f2a1e1465c5757d299
Parkwochang <whwh3560@gmail.com>
2022-07-21 23:16:05 +0900

    polymorphism

--- description.ts
+++ description.ts
@@ -76,3 +76,31 @@ abstract class Userk {
   abstract sayHi(name: string): string;
   abstract fullName(): string;
 }
+
+interface SStorage<T> {
+  [key: string]: T;
+}
+
+class LocalStorage<T> {
+  private storage: SStorage<T> = {};
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
+
+const stringsStorage = new LocalStorage<string>();
+
+stringsStorage.get("ket");
+
+const booleansStorage = new LocalStorage<boolean>();
+
+booleansStorage.get("xxx");
