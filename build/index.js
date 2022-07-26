e3088110cf4f7c9aedca17602dc88ef7adcf66f1
Parkwochang <whwh3560@gmail.com>
2022-07-26 23:14:34 +0900

    blocks

--- build/index.js
+++ build/index.js
@@ -1,6 +1,13 @@
-import { init, exit } from "myPackage";
-init({
-    url: "true",
-});
-exit(1);
-localStorage.clear();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Block {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
+        this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+    }
+}
